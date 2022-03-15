07f145272631a867d3f765b628113cb8fee8f61f
Rafael Negron <r.negron1@umiami.edu>
2022-03-15 14:04:26 -0400

    Adding Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,6 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier",
   ],
 };
