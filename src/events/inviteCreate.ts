56f724b2e47c7acc2014bdaf32c2115cd8708297
John Downey <johntdowney@gmail.com>
2022-03-20 17:55:29 -0600

    No, I think THIS is prettier.

--- src/events/inviteCreate.ts
+++ src/events/inviteCreate.ts
@@ -5,9 +5,7 @@ import { IEvent } from "../interfaces/IEvent.interface";
 module.exports = {
   execute(invite: Invite) {
     console.log(
-      `User #${invite?.inviterId || "???"} created an invite ${
-        invite?.code || "???"
-      }.`
+      `User #${invite?.inviterId || "???"} created an invite ${invite?.code || "???"}.`
     );
     // TODO: Store these invites
   },
