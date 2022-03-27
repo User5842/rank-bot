e5b388eedee02f6f13843226954ba91842648567
John Downey <johntdowney@gmail.com>
2022-03-27 14:44:18 -0600

    fixed intents to be consistent with the repo pattern

--- src/utils/DiscordClientInstance.ts
+++ src/utils/DiscordClientInstance.ts
@@ -1,12 +1,7 @@
 import { Client, Intents } from "discord.js";
 
 const client = new Client({
-  intents: [
-    Intents.FLAGS.GUILDS,
-    Intents.FLAGS.GUILD_MEMBERS,
-    Intents.FLAGS.GUILD_MESSAGES,
-    Intents.FLAGS.GUILD_INVITES,
-  ],
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 
 export default client;
