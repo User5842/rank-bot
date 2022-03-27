96bf4d06b87d52d34de2504373a31bfccfc3d6a8
John Downey <johntdowney@gmail.com>
2022-03-27 14:49:03 -0600

    Added an event listener for guildMemberAdd.

--- src/utils/DiscordClientInstance.ts
+++ src/utils/DiscordClientInstance.ts
@@ -1,7 +1,7 @@
 import { Client, Intents } from "discord.js";
 
 const client = new Client({
-  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES],
 });
 
 export default client;
